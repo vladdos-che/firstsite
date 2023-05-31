b64b066eccf14c1b6aa3a66d7ec1872d72f50d7b
Владислав Чебанов <@itstep.loc>
2023-05-31 17:40:53 +0600

    lesson_12 end

---
+++ bboard/templatetags/count_bbs.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='count_bbs')
+def count_bbs(count_bb, pk):
+    return count_bb.get(pk)
--- bboard/views.py
+++ bboard/views.py
@@ -1,9 +1,18 @@
+from django.db.models import Min, Max, Count, Q, Sum, IntegerField, Avg
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from bboard.forms import BbForm
 from bboard.models import Bb, Rubric
+
+
+def count_bb():
+    result = dict()
+    for r in Rubric.objects.annotate(num_bbs=Count('bb')):
+        result.update({r.pk: r.num_bbs})
+
+    return result
 
 
 class BbCreateView(CreateView):
@@ -14,15 +23,63 @@ class BbCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
+        context['count_bb'] = count_bb()
         return context
 
 
 def index(request):
     bbs = Bb.objects.order_by('-published')
     rubrics = Rubric.objects.all()
+
+    # min_price = Bb.objects.aggregate(Min('price'))
+    # max_price = Bb.objects.aggregate(mp=Max('price'))
+    result = Bb.objects.aggregate(min_price=Min('price'),
+                                  max_price=Max('price'),
+                                  diff_price=Max('price') - Min('price'),)
+
+    # for r in Rubric.objects.annotate(Count('bb')):
+    #     print(f'{r.name}: {r.bb__count}')
+    #
+    # for r in Rubric.objects.annotate(num_bbs=Count('bb')):
+    #     print(f'{r.name}: {r.num_bbs}')
+
+    # for r in Rubric.objects.annotate(cnt=Count('bb',
+    #                                            filter=Q(bb__price__gt=100_000)
+    #                                            # min=Min('bb__price')).filter(cnt__gt=0)
+    #                                            )):
+    #     # print(f'{r.name}: {r.min}')
+    #     print(f'{r.name}: {r.cnt}')
+
+    # print(
+    #     Bb.objects.aggregate(
+    #         sum=Sum(
+    #             'price',
+    #             output_field=IntegerField(),
+    #             filter=Q(rubric__name='Бытовая техника')
+    #         )
+    #     )
+    # )
+
+    # print(
+    #     Bb.objects.aggregate(
+    #         avg=Avg(
+    #             'price',
+    #             output_field=IntegerField(),
+    #             filter=Q(rubric__name='Сельхозтехника'),
+    #             distinct=False  # True = только уникальные
+    #         )
+    #     )
+    # )
+
     context = {
         'bbs': bbs,
         'rubrics': rubrics,
+        # 'min_price': min_price.get('price__min'),
+        # 'max_price': max_price.get('mp'),
+        'min_price': result.get('min_price'),
+        'max_price': result.get('min_price'),
+        'diff_price': result.get('diff_price'),
+        'count_bb': count_bb(),
     }
     return render(request, 'bboard/index.html', context)
 
@@ -34,7 +91,8 @@ def by_rubric(request, rubric_id):
     context = {
         'bbs': bbs,
         'rubrics': rubrics,
-        'current_rubric': current_rubric
+        'current_rubric': current_rubric,
+        'count_bb': count_bb(),
     }
     return render(request, 'bboard/by_rubric.html', context)
 
@@ -43,5 +101,7 @@ def login(request):
     rubrics = Rubric.objects.all()
     context = {
         'rubrics': rubrics,
+        'count_bb': count_bb(),
+
     }
     return render(request, 'bboard/login.html', context)
