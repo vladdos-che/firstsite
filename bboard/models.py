2b3a7c50d83b9c9e2a555624f246e9c47908f55d
vladdos-che <vlad1010che@gmail.com>
2023-05-31 11:20:00 +0600

    lesson_08_hw end

--- bboard/models.py
+++ bboard/models.py
@@ -116,3 +116,168 @@ class Bb(models.Model):
         verbose_name = "Объявление"
         verbose_name_plural = "Объявления"
         ordering = ['-published', 'title']
+
+
+def positive_num_validator(val):
+    if val < 0:
+        raise ValidationError(f'Число {val} меньше 0', code='negative')
+
+
+class IceCreamKiosk(models.Model):
+    name = models.CharField(
+        max_length=20,
+        db_index=True,
+        verbose_name="Название киоска",
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_sum_id(self):
+        return self.pk + self.pk
+
+    class Meta:
+        verbose_name = 'Киоск с мороженным'
+        verbose_name_plural = 'Киоски с мороженным'
+        ordering = ['name']
+
+
+class IceCream(models.Model):
+    kiosk = models.ForeignKey(
+        'IceCreamKiosk',
+        null=True,
+        on_delete=models.PROTECT,
+        verbose_name='Рубрика',
+    )
+
+    title = models.CharField(
+        max_length=50,
+        verbose_name="Название мороженого",
+    )
+
+    content = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание",
+    )
+
+    price = models.FloatField(
+        null=True,
+        blank=True,
+        verbose_name="Цена",
+        validators=[positive_num_validator],
+    )
+
+    published = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name="Добавлено",
+    )
+
+    compound = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Состав",
+        default="Секрет фирмы",
+    )
+
+    quantity = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Количество",
+        default=0,
+    )
+
+    def __str__(self):
+        return f'Мороженое: {self.title}'
+
+    def get_sum_id_price(self):
+        return self.pk + self.price
+
+    class Meta:
+        verbose_name = "Мороженое"
+        verbose_name_plural = "Мороженые"
+        ordering = ['-published', 'title']
+
+
+class Parent(models.Model):
+    name = models.CharField(
+        max_length=30,
+        verbose_name="Имя",
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание",
+    )
+
+    was_born = models.DateTimeField(
+        db_index=True,
+        null=True,
+        blank=True,
+        verbose_name="Родился",
+    )
+
+    age = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Возраст",
+        default=0,
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_sum_id(self):
+        return self.pk + self.pk
+
+    class Meta:
+        verbose_name = "Родитель"
+        verbose_name_plural = "Родители"
+        ordering = ['-age', 'name']
+
+
+class Child(models.Model):
+    parent = models.ForeignKey(
+        'Parent',
+        null=True,
+        on_delete=models.PROTECT,
+        verbose_name='Родитель',
+    )
+
+    name = models.CharField(
+        max_length=30,
+        verbose_name="Имя",
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание",
+    )
+
+    was_born = models.DateTimeField(
+        db_index=True,
+        null=True,
+        blank=True,
+        verbose_name="Родился",
+    )
+
+    age = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Возраст",
+        default=0,
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_sum_id(self):
+        return self.parent.pk + self.pk
+
+    class Meta:
+        verbose_name = "Ребёнок"
+        verbose_name_plural = "Дети"
+        ordering = ['-age', 'name']
