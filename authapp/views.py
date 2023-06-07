3afcafcc7721066ebe8311f06fe2d0bb94d02ded
Владислав Чебанов <@itstep.loc>
2023-06-07 17:25:34 +0600

    lesson_14 files

--- authapp/views.py
+++ authapp/views.py
@@ -3,9 +3,29 @@ from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
+from authapp.forms import UserLoginForm
+
 
 def login(request):
-    pass
+    title = "Login"
+    login_form = UserLoginForm(data=request.POST)
+
+    if request.method == "POST" and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+    context = {
+        'title': title,
+        'login_form': login_form,
+    }
+
+    return render(request, 'authapp/login.html', context)
 
 
 def logout(request):
