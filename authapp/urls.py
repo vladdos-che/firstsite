e39ec26529e9aa28b400febc580478113d438455
Владислав Чебанов <@itstep.loc>
2023-08-16 17:14:16 +0600

    lesson_26 pract

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from authapp.views import login, logout, UserListView, UserByNameView
+from authapp.views import login, logout, UserListView, UserByNameView, register
 
 app_name = "authapp"
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('register/', register, name='register'),
     path('user/', UserByNameView.as_view(), name='user'),  # lesson_19_hw
     path('userlist/', UserListView.as_view(), name='user_list'),  # lesson_19_hw
 ]
