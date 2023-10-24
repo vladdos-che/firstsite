e7b521c521b947430679dc9ddcca06a5d2b270c0
unknown <Zver@Gigabyte.rkh.kz>
2023-10-24 17:42:30 +0600

    lesson_53 mid

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.2.3.4',
@@ -208,7 +208,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+
+STATIC_URL = 'static/'
 
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static'),
