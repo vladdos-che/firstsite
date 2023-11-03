d922caf6d300b13afa2cd3529e8ab1a9e62dad73
vlad-che <vlad1010che@gmail.com>
2023-11-03 16:44:44 +0600

    lesson_52_hw

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -206,11 +206,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
